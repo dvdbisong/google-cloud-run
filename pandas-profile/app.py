605815791ca2eedfab9053ee1b91d9a0d48844cc
Ekaba Bisong <dvdbisong@gmail.com>
2019-07-06 02:08:47 -0400

    update app.py

--- pandas-profile/app.py
+++ pandas-profile/app.py
@@ -7,23 +7,12 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    target = os.environ.get('TARGET', 'World')
-    return 'Hello {}!\n'.format(target)
-
-@app.route('/profile')
 def profile():
     # importing the data
     df = pd.read_csv(request.args.get('data'))
     profile = pandas_profiling.ProfileReport(df)
     profile.to_file(output_file="templates/output.html")
     return render_template('output.html')
-
-@app.route('/query-example')
-def query_example():
-    language = request.args.get('language') #if key doesn't exist, returns None
-
-    return '''<h1>The language value is: {}</h1>'''.format(language)
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
