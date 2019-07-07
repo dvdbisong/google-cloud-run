5e412a5a83213f2126d8e02ea2c330fb26b98758
Ekaba Bisong <dvdbisong@gmail.com>
2019-07-07 15:43:31 -0400

    update app.py

--- pandas-profile/app.py
+++ pandas-profile/app.py
@@ -7,7 +7,7 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def home():
     return render_template('index.html')
 
 @app.route('/profile')
